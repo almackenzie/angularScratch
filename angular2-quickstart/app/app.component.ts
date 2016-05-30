db81923c6e34e39bf40601fca09f732ae5271b88
Al Mackenzie <alastair.mackenzie82@gmail.com>
2016-05-30 21:57:52 +0100

    next stages ... split into components and services

--- angular2-quickstart/app/app.component.ts
+++ angular2-quickstart/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core'
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+
 @Component({
 	selector: 'my-app',
 	template: `
@@ -11,14 +16,7 @@ import { Component } from '@angular/core'
 		    <span class="badge">{{hero.id}}</span> {{hero.name}}
 		  </li>
 		</ul>
-		<div *ngIf="selectedHero">
-	  		<h2>{{selectedHero.name}} details!</h2>
-	  		<div><label>id: </label>{{selectedHero.id}}</div>
-	  		<div>
-	  			<label>name: </label>
-	  			<input [(ngModel)]="selectedHero.name" placeholder="name" />
-	  		</div>
-  		</div>
+		<my-hero-detail [hero]="selectedHero"></my-hero-detail>
   		`,
   	styles: [`
 		  .selected {
@@ -68,28 +66,22 @@ import { Component } from '@angular/core'
 		    margin-right: .8em;
 		    border-radius: 4px 0 0 4px;
 		  }
-	`]
+	`],
+	directives: [HeroDetailComponent],
+	providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	constructor(private heroService: HeroService) { }
 	title = 'Tour of Heroes';
 	selectedHero: Hero;
 	public heroes = HEROES;
 	onSelect(hero: Hero) { this.selectedHero = hero; }
-}
-export class Hero {
-	id: number;
-	name: string;
+	getHeroes() {
+		this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+	}
+	ngOnInit() {
+		this.getHeroes();
+	}
 }
 
-var HEROES: Hero[] = [
-	{ "id": 11, "name": "Mr. Nice" },
-	{ "id": 12, "name": "Narco" },
-	{ "id": 13, "name": "Bombasto" },
-	{ "id": 14, "name": "Celeritas" },
-	{ "id": 15, "name": "Magneta" },
-	{ "id": 16, "name": "RubberMan" },
-	{ "id": 17, "name": "Dynama" },
-	{ "id": 18, "name": "Dr IQ" },
-	{ "id": 19, "name": "Magma" },
-	{ "id": 20, "name": "Tornado" }
-];
+var HEROES: Hero[];
